31ac4a21743e57ef706fc57aa73fb0d2545a37c6
ashejsx <julioastudillo3150@gmail.com>
2025-06-05 11:55:55 -0400

    Light mode for Resume button

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -6,7 +6,19 @@ function MyButton({ url }) {
   };
 
   return (
-    <button className='bg-dark dark:bg-dark-2 border-dark dark:border-dark-2 border rounded-full inline-flex items-center justify-center py-3 px-7 text-center text-base font-medium text-white hover:bg-body-color hover:border-body-color disabled:bg-gray-3 disabled:border-gray-3 disabled:text-dark-5' onClick={handleClick}>
+    <button
+      className="
+        border-2 border-dark text-dark
+        dark:border-dark-2 dark:text-white
+        bg-transparent dark:bg-dark-2
+        rounded-full inline-flex items-center justify-center
+        py-3 px-7 text-center text-base font-medium
+        hover:bg-body-color hover:border-body-color
+        disabled:bg-gray-3 disabled:border-gray-3 disabled:text-dark-5
+        transition-colors
+      "
+      onClick={handleClick}
+    >
       Get my resume
     </button>
   );
